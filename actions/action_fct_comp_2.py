9d5f4a5d955bc1d019d41a18e38e97a95b28ffc9
charlie38 <38466672+charlie38@users.noreply.github.com>
2019-11-24 22:11:21 +0100

    TODO 1.6

--- actions/action_fct_comp_2.py
+++ actions/action_fct_comp_2.py
@@ -14,20 +14,28 @@ class AppFctComp2(QDialog):
         self.ui = uic.loadUi("gui/fct_comp_2.ui", self)
         self.data = data
 
-    # Fonction de mise à joru de l'affichage
+    # Fonction de mise à jour de l'affichage
     @pyqtSlot()
     def refreshResult(self):
         # TODO 1.5 : fonction à modifier pour remplacer la zone de saisie par une liste de valeurs prédéfinies dans l'interface une fois le fichier ui correspondant mis à jour
         display.refreshLabel(self.ui.label_fct_comp_2, "")
-        if not self.ui.lineEdit_fct_comp_2.text().strip():
+
+        button_binding = None
+        if self.ui.radioButton.isChecked():
+            button_binding = self.ui.radioButton.text()
+        if self.ui.radioButton_2.isChecked():
+            button_binding = self.ui.radioButton_2.text()
+        if self.ui.radioButton_3.isChecked():
+            button_binding = self.ui.radioButton_3.text()
+
+        if button_binding is None :
             self.ui.table_fct_comp_2.setRowCount(0)
-            display.refreshLabel(self.ui.label_fct_comp_2, "Veuillez indiquer un nom de catégorie")
-        else:
+            display.refreshLabel(self.ui.label_fct_comp_2, "Veuillez sélectionner un nom de catégorie")
+        else :
             try:
                 cursor = self.data.cursor()
-                result = cursor.execute(
-                    "SELECT noPlace, noRang, noZone FROM LesZones NATURAL JOIN LesPlaces WHERE catZone = ?",
-                    [self.ui.lineEdit_fct_comp_2.text().strip()])
+                result = cursor.execute("SELECT noPlace, noRang, noZone FROM LesZones NATURAL JOIN LesPlaces WHERE catZone = ?",
+                                        [button_binding])
             except Exception as e:
                 self.ui.table_fct_comp_2.setRowCount(0)
                 display.refreshLabel(self.ui.label_fct_comp_2, "Impossible d'afficher les résultats : " + repr(e))
